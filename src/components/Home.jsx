21c5ec49dccdd4f93eed63e39890a2b02093824c
Duraimurugan-H <hduraimurugan@gmail.com>
2024-09-26 16:25:31 +0530

    Ui changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,12 +4,21 @@ import HeroSection from "./HeroSection"
 import LatestJobs from "./LatestJobs"
 import Footer from "./shared/Footer"
 import Navbar from "./shared/Navbar"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
 import { useEffect } from "react"
+import { setSearchedQuery } from "@/redux/jobSlice"
 
 const Home = () => {
   useGetAllJobs();
+  
+  const dispatch = useDispatch();
+  useEffect(()=>{
+      return ()=>{
+          dispatch(setSearchedQuery(""));
+      }
+  },[])
+
   const { user } = useSelector(store => store.auth);
   const navigate = useNavigate();
   useEffect(() => {
